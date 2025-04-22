c82ce39d61e50f3e68c4298567cffaa7754ea370
Puk06 <86549420+puk06@users.noreply.github.com>
2025-04-23 08:36:54 +0900

    v1.0.10にアップデート

--- Forms/Main.cs
+++ Forms/Main.cs
@@ -16,7 +16,7 @@ namespace Avatar_Explorer.Forms
         /// <summary>
         /// ソフトの現在のバージョン
         /// </summary>
-        private const string CurrentVersion = "v1.0.9";
+        private const string CurrentVersion = "v1.0.10";
 
         /// <summary>
         /// デフォルトのフォームテキスト
