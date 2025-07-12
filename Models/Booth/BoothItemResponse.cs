acebe7740a813f68f02ea41a4d0b9108f25d81a6
Puk06 <86549420+puk06@users.noreply.github.com>
2025-07-12 19:10:42 +0900

    BoothのModelファイルに空行の追加

--- Models/Booth/BoothItemResponse.cs
+++ Models/Booth/BoothItemResponse.cs
@@ -3,7 +3,10 @@
 public class BoothItemResponse
 {
     public string Name { get; set; } = string.Empty;
+
     public ShopInfo Shop { get; set; } = new ShopInfo();
+
     public List<ImageInfo> Images { get; set; } = new List<ImageInfo>();
+
     public CategoryInfo Category { get; set; } = new CategoryInfo();
 }
